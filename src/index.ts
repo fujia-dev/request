b7fc027b30b16fbcfc40e6393c89959e82098e9e
fujia <fujia.site@outlook.com>
2022-03-26 21:30:05 +0800

    feat: encapsulated base case

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export const demo = () => {
-  // NOTE: do somethings
-};
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+class Request {
+  instance: AxiosInstance;
+
+  constructor(config: AxiosRequestConfig) {
+    this.instance = axios.create(config);
+  }
+
+  request(config: AxiosRequestConfig) {
+    return this.instance.request(config);
+  }
+}
+
+export default Request;
